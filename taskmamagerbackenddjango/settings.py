318da91b376232173bab9cba338e7e54450cb347
Rohit Satpute <rohitsatpute480@gmail.com>
2025-04-19 21:39:57 +0530

    added frontent in cors host

--- taskmamagerbackenddjango/settings.py
+++ taskmamagerbackenddjango/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    'https://task-manager-frontend-tawny-one.vercel.app'
 ]
 
 ROOT_URLCONF = 'taskmamagerbackenddjango.urls'
