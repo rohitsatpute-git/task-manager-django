f38cd307ef10583066c5cd377773f020e244f826
Rohit Satpute <rohitsatpute480@gmail.com>
2025-04-20 00:04:12 +0530

    changed db

--- taskmamagerbackenddjango/settings.py
+++ taskmamagerbackenddjango/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'taskmamagerbackenddjango.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
